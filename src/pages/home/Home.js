e2f11052174a287d9f7a7af0ec3387b5950f45a9
Muhammad Hamza <hamzashafique535@gmail.com>
2022-03-03 17:22:45 +0500

    To Do list navbar set

---
+++ src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PrimarySearchAppBar from '../../component/primarySearchAppBar/PrimarySearchAppBar';
+import PersistentDrawerLeft from '../../component/mainContentWithDrawer/PersistentDrawerLeft';
+
+export default function Home() {
+    return (
+        <>
+        <PrimarySearchAppBar />
+        <PersistentDrawerLeft />
+           
+            
+
+
+        </>
+    )
+}
